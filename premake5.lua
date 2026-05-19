3567e7bb600762cafb61495bb56e07a938b5be65
KeshavShrimali-Capsitech <keshav.shrimali@capsitech.com>
2026-05-19 19:43:02 +0530

    Fix typo and add imgui_tables.cpp to project files
    Fixed a typo in targetdir and added imgui_tables.cpp to files.

--- premake5.lua
+++ premake5.lua
@@ -1,40 +1,35 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
+  kind "StaticLib"
+  language "C++"
+  cppdialect "C++17"
+  staticruntime "On"
+  toolset "v145"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  -- Fixed the typo here: changed %(prj.name} to %{prj.name}
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp",
-    "imgui_tables.cpp"
-	}
+  files {
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_tables.cpp", -- Added this line!
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp"
+  }
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
+  filter "system:windows"
+    systemversion "latest"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+  
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
